974e1692fd0a17712abe7babacee7202368dc67a
MSI\User <wilsonngja@gmail.com>
2022-09-16 17:00:25 +0800

    Add explanatory comment

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -15,8 +15,11 @@ public class Main {
         DataManager dm = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dm.loadData();
 
-        System.out.println("Printing deadlines");
-        printDeadlines(tasksData);
+//        System.out.println("All data");
+//        printData(tasksData);
+//
+//        System.out.println("Printing deadlines");
+//        printDeadlines(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
